7677172cada33d23d01b7d89c661fb75cb74dfa2
Edward Valentine <eddieval2022@gmail.com>
2022-08-03 20:47:26 +0100

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import { HeaderStyle, HeaderH1, HeaderH2 } from "./headersyled";
+
+const Header = () => {
+  return (
+    <HeaderStyle>
+      <HeaderH1>React practice</HeaderH1>
+      <HeaderH2>Developed by Kat & Eddie</HeaderH2>
+    </HeaderStyle>
+  );
+};
+
+export default Header;
---
+++ src/components/navbar.js
@@ -0,0 +1,37 @@
+import { Link, Navigate } from "react-router-dom";
+import { LiStyled, NavStyled } from "./navbarstyled";
+
+const Navbar = ({ user, setter }) => {
+  return (
+    <nav>
+      {!user && <Navigate to="/" />}
+      
+      <NavStyled>
+      
+        <LiStyled>
+          <Link to="/home">Home</Link>
+        </LiStyled>
+        <LiStyled>
+          <Link to="/profile">Profile</Link>
+        </LiStyled>
+        <LiStyled>
+          <Link to="/feed">Feed</Link>
+        </LiStyled>
+        <LiStyled>
+          <Link to="/contests">Contest</Link>
+        </LiStyled>
+        <LiStyled
+          onClick={() => {
+            setter();
+            localStorage.removeItem("myToken");
+          }}
+        >
+          Log Out
+        </LiStyled>
+      
+    </NavStyled>
+    </nav>
+  );
+};
+
+export default Navbar;
