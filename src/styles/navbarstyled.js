42889382d32cd10eb01598c69dff0d4134cd3d01
Edward Valentine <eddieval2022@gmail.com>
2022-08-04 00:17:31 +0100

    fix2

--- src/styles/navbarstyled.js
+++ src/styles/navbarstyled.js
@@ -11,7 +11,7 @@ export const NavStyled = styled.div`
   display: flex;
   justify-content: space-between;
   flex-direction: row;
-  cursor: pointer;
+ 
   margin: 45px;
   font-size: 20px;
   font-weight: bold;
